aafececf995081ce53859b0f9abbf5bcd87bdd08
Eivind Egholm Vørmadal <eev_98@hotmail.com>
2023-06-17 22:56:51 +0200

    Add project files.

---
+++ RevitFamiliesDb/RevitFamiliesDb/App.cs
@@ -0,0 +1,41 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+#endregion
+
+namespace RevitFamiliesDb
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Create a custom ribbon tab
+            var tabName = "Eivind";
+            application.CreateRibbonTab(tabName);
+
+            // Add a new ribbon panel
+            RibbonPanel panel = application.CreateRibbonPanel(tabName, "IFC Tools");
+
+            new ButtonBuilder("Export IFC", typeof(Command))
+                .Text("Hello Eivind")
+                .Tooltip("Go to 3D View you want to export")
+                .ImagePath("C:\\Users\\eev_9\\source\\repos\\Eivind98\\EivindsAddins\\ExampleAddIn\\Logos\\Door Orientation.png")
+                .Build(panel);
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ RevitFamiliesDb/RevitFamiliesDb/Command.cs
@@ -0,0 +1,100 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+#endregion
+
+namespace RevitFamiliesDb
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            var uiapp = commandData.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var app = uiapp.Application;
+            var doc = uidoc.Document;
+
+            // Access current selection
+
+            var sel = uidoc.Selection;
+
+
+            var elId = sel.GetElementIds().FirstOrDefault();
+
+            if (elId == null) return Result.Succeeded;
+
+
+            // Retrieve elements from database
+            var element = new FilteredElementCollector(doc)
+                .WhereElementIsElementType()
+                .FirstOrDefault(x => x.Id == elId) as FloorType;
+
+            //var dialog = new TaskDialog($"IsValidObject: {sel.IsValidObject}, ElementIdCount: {sel.GetElementIds().Count}");
+            var dialog = new TaskDialog("Debug");
+            //dialog.MainContent = json;
+
+
+            var newElementId = Guid.NewGuid().ToString();
+            using (var tx = new Transaction(doc))
+            {
+                tx.Start("Douche bag");
+                var newElement = element.Duplicate(newElementId) as FloorType;
+                var structure = CompoundStructure.CreateSimpleCompoundStructure(new List<CompoundStructureLayer>()
+                {
+                    new CompoundStructureLayer(100, MaterialFunctionAssignment.Structure, new ElementId(BuiltInCategory.INVALID))
+                });
+                structure.EndCap = EndCapCondition.NoEndCap;
+
+                IDictionary<int, CompoundStructureError> errors = new Dictionary<int, CompoundStructureError>();
+                IDictionary<int, int> twoErrors = new Dictionary<int, int>();
+                structure.IsValid(doc, out errors, out twoErrors);
+
+                foreach (Parameter param in newElement.Parameters)
+                {
+
+                    param.SetValueString("");
+
+                }
+
+                //dialog.MainContent = newElement.Location?.ToString();
+                dialog.MainContent = JsonConvert.SerializeObject(errors, Formatting.Indented);
+                dialog.Show();
+                newElement.SetCompoundStructure(structure);
+                tx.Commit();
+            }
+
+
+            //var test = JsonConvert.DeserializeObject<CompoundStructure>(json);
+
+            // Filtered element collector is iterable
+
+            //foreach (Element e in col)
+            //{
+            //    Debug.Print(e.Name);
+            //}
+
+            // Modify document within a transaction
+
+            //using (var tx = new Transaction(doc))
+            //{
+            //    tx.Start("Transaction Name");
+            //    tx.Commit();
+            //}
+
+            return Result.Succeeded;
+        }
+    }
+}
